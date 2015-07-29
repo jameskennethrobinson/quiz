2f9cac5d2141db2d1764f736402fa991700a74d1
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-07-29 11:38:39 -0700

    algo now returns correct word there is a call stack leak somewhere which is preventing the algo from returning at the proper time

--- word.js
+++ word.js
@@ -7,20 +7,25 @@ var words = function(list){
 	  return b.length - a.length; 
 	});
 
+	var ans;
+
 	var ind = 0;
 
-	return (function recurse (str, frag, end){
+	(function recurse (str, frag, end){
+
+		if (sorted[ind] === undefined) return;
+
+		if (frag === sorted[ind]){
+			console.log('found ------', frag);
+			ans = frag;
+			return;
+		}
 
 		if (end === sorted[ind].length){
 			console.log('not found');
 			ind++;
 			recurse(sorted[ind], "", 1)
 		} 
-
-		if (frag === sorted[ind]){
-			console.log('found ------', frag);
-			return frag;
-		}
 
 		var substr = str.slice(0, end);
 
@@ -36,9 +41,10 @@ var words = function(list){
 
 	})(sorted[ind], "", 1)
 
+	return ans;
 };
 
 
 
 var list = ['cat', 'dog', 'catdog', 'catdogcat', 'catlkdajflkfajk', '1234', 'ladjf;lajksdf;lajskdf;laksdjfla;sdkf'];
-words(list);
+console.log(words(list));
