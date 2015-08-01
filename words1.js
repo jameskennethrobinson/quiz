c7667618e15865a0ab78e0ab55a33adf1a3b5cc2
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-08-01 11:53:57 -0700

    working iterative-recursive solution does not check through the array if largest word fails

--- words1.js
+++ words1.js
@@ -1,31 +1,28 @@
 var words = function (list) {
 
+	var obj = {};
+	var ans;
+	//could populate object and sort at the same time
 	var sorted = Array.prototype.slice.apply(list).sort(function(a, b){
 	  return b.length - a.length; 
 	});
-
-	var obj = {};
-	var ans;
 
 	sorted.forEach(function(item){
 		obj[item] = item;
 	});
 
-	//console.log(obj)
-
-
 	function recurse (str, frag) {
 
-		console.log('fragment - ', frag, ' : string - ', sorted[0]);
+		console.log('fragment - ', frag, ' : string - ', str);
 
-
-		if (obj[str]){
-			delete obj[str];
+		if (obj[sorted[0]]){
+			delete obj[sorted[0]];
 		}
 
 		if (frag === sorted[0]){
 			console.log('answer found');
 			var ans = frag;
+			console.log(ans)
 			return;
 		}
 
@@ -33,8 +30,10 @@ var words = function (list) {
 
 			var substr = str.slice(0, i);
 
+			console.log('substr - ', substr)
+
 			if (obj.hasOwnProperty(substr)){
-				console.log('fragment found - ', substr)
+				console.log('fragment found -', substr)
 				var str = str.split("");
 				var removed = str.splice(0, i).join("");
 				recurse(str.join(""), frag.concat(removed))
@@ -43,34 +42,15 @@ var words = function (list) {
 
 		}
 
-		console.log('This word is not a compound word');
+		console.log(sorted[0], 'is not a compound word');
 		return;
-
-
-		//var substr = str.slice(0, end);
-
-		//var ind = sorted.indexOf(substr);
-		//var bool = obj.hasOwnProperty(substr);
-
-		//console.log(substr);
-		//console.log(bool);
-
-		// if (!bool){
-		// 	//recurse(str, frag, ++end);
-		// }
-
-
-		// else{
-		// 	var str = str.split("");
-		// 	var removed = str.splice(0, end).join("");
-		// 	recurse(str.join(""), frag.concat(removed), 0)	
-		// }
 	}
 
 	recurse(sorted[0], "");
+	console.log(ans);
 	return ans
 
-}
+};
 
 var list = ['cat', 'dog', 'catdog', 'catdogcat'];
 
