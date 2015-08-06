aa3da77c9b380bf19dcbfb13727a2026d23253f8
JamesKennethRobinson <Jameskennthrobinson@gmail.com>
2015-08-06 12:10:50 -0700

    working on iterating through possible words odd bug in iteration

--- findLargest.js
+++ findLargest.js
@@ -2,13 +2,13 @@ exports.findLargestCompound = function (list) {
 
 	var obj = {};
 	var ans;
-	//var ind = 0;
+	var ind = 0;
 
 	//could populate object and sort at the same time
 	var sorted = Array.prototype.slice.apply(list).sort(function(a, b){
 	  return b.length - a.length; 
 	});
-	//console.log(sorted)
+	console.log(sorted)
 
 	sorted.forEach(function(item){
 		obj[item] = item;
@@ -16,31 +16,31 @@ exports.findLargestCompound = function (list) {
 
 	function recurse (str, frag) {
 
-		// if (ind === sorted.length){
-		// 	console.log('No compounds words in this list');
-		// 	return;
-		// }
-
-		console.log('fragment - ', frag)
-
-		if (obj[sorted[0]]){
-			delete obj[sorted[0]];
+		if (ind === sorted.length){
+			console.log('No compounds words in this list');
+			return;
 		}
 
-		if (frag === sorted[0]){
+		//console.log('-------------')
+
+		//console.log('fragment - ', frag)
+
+
+		if (obj[sorted[ind]]){
+			delete obj[sorted[ind]];
+		}
+
+		if (frag === sorted[ind]){
 			console.log('answer found: ', frag);
 			ans = frag;
 			return;
 		}
 
-		if (!str) return;
-
-
 
 
 		for (var i=1; i<=str.length; i++){
-
 			
+		
 			var substr = str.slice(0, i);
 			//console.log('WHOLE - ', str);
 			//console.log('CONSIDERED:', substr)
@@ -48,10 +48,10 @@ exports.findLargestCompound = function (list) {
 			if (obj.hasOwnProperty(substr)){ //&& i<=str.length){
 
 
-				console.log('FOUND - ', substr)
+				//console.log('FOUND - ', substr)
 				var remainder = str.split("").splice(i).join("");
 
-				console.log('REMAINDER - ',remainder)
+				//console.log('REMAINDER - ',remainder)
 				
 				recurse(remainder, frag.concat(substr))
 		
@@ -59,18 +59,30 @@ exports.findLargestCompound = function (list) {
 			
 
 		}
+		
 
 		//console.log(sorted[0], "is not a compound word")
 		//++ind;
 		//recurse(sorted[ind], "");
 	}
 
-	recurse(sorted[0], "");
+
+
+	
+
+	if (!ans){
+		console.log(sorted[ind], 'is not a compound word');
+		++ind;
+		recurse(sorted[ind], "");
+		
+	}
+
+	console.log(ans, 'is the answer')
 
 	return ans
 
 };
 
-var list = ['catar', 'c', 'cat', 'ar'];
-var list1 = ['catar', 'c', 'atar', 'cat']
-exports.findLargestCompound(list1)
+var list = ['catar', 'c', 'cat', 'ar', 'asdfasdfasdfasdf', 'hellohellohellohello'];
+var list1 = ['catar', 'c', 'atar', 'cat', 'asdfasdfasdf']
+exports.findLargestCompound(list)
